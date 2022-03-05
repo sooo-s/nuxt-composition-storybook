0904f18489d6e599cf4654b238174ceedb47a272
sooo-s <mail@sooo.space>
2022-03-05 22:50:46 +0900

    transpile mjs in storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -7,5 +7,21 @@ module.exports = {
     "@storybook/addon-links",
     "@storybook/addon-essentials"
   ],
-  "framework": "@storybook/vue"
+  "framework": "@storybook/vue",
+  webpackFinal: (config) => {
+    config.module.rules.forEach(rule => {
+      if (rule.test instanceof RegExp && rule.test.test('index.mjs')) {
+        rule.type = 'javascript/auto'
+      }
+    })
+
+    config.module.rules.unshift({
+      test: /\.mjs$/,
+      type: 'javascript/auto',
+      include: [/node_modules/],
+    })
+    config.node  = { fs: 'empty' };
+
+    return config;
+  },
 }
