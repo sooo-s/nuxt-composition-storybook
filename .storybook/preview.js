fffa52a0011ad390ce5c0bab32effa2f2a2c3a81
sooo-s <mail@sooo.space>
2022-03-06 22:18:28 +0900

    Vue.use composition-api

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,7 @@
+import Vue from 'vue'
+import VueCompositionApi from '@vue/composition-api'
+Vue.use(VueCompositionApi)
+
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
